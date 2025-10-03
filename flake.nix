56b857b43c6298f0cec561e7c4e60b8af47c748f
radicaltray <radicaltray@gmail.com>
2025-10-03 22:11:32 +0700

    add datasets probably violated some license but im sure we're not going to jail

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+            ] ++ (with pkgs.python313Packages; [
+              jupyterlab pandas python-dotenv openai kaggle
+            ]);
+          };
+        }
+      );
+    };
+}
